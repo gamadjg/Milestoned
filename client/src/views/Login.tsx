ef1cfddb1f84a07e63e7463ed7439e9f8ae5b971
gamadjg <gamadavid36@gmail.com>
2023-11-29 10:06:23 -0800

    Login and register pages complete

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -36,7 +36,7 @@ export default function Login() {
     };
 
     return (
-        <main className="flex w-screen h-screen flex-col items-center justify-center gap-9 px-3 md:px-52">
+        <main className="flex w-full h-full flex-col items-center justify-center gap-9 px-3 md:px-52">
             <p className="text-2xl">Login</p>
             <div className="w-5/6 md:w-3/6">
                 <LoginRegForm login handleUser={handleLogin} />
--- client/src/views/Register.tsx
+++ client/src/views/Register.tsx
@@ -43,8 +43,9 @@ export default function Register() {
     };
 
     return (
-        <main className="flex w-screen h-screen flex-col items-center justify-center gap-9 px-3 md:px-52">
-            <p className="text-2xl">Register here to create an account!</p>
+        <main className="flex w-full h-full flex-col items-center justify-center gap-9 px-3 md:px-52">
+            {/* // <main className="w-full h-full grid items-center justify-center gap-9 px-3 md:px-52"> */}
+            <p className="text-2xl">Register</p>
             <div className="w-5/6 md:w-3/6">
                 <LoginRegForm register handleUser={handleRegister} />
             </div>
