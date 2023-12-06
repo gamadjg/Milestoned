7f79379b27b25592bafa035af554c8d91c587123
gamadjg <gamadavid36@gmail.com>
2023-12-05 20:33:25 -0800

    Edited milestone sorting for different years. Added Line next to milestones in list

--- client/src/components/MilestoneList.tsx
+++ client/src/components/MilestoneList.tsx
@@ -8,15 +8,15 @@ const MilestoneList = ({ milestones }: Props) => {
     console.log("milestones", milestones);
     let sortedMilestones: Milestone[] = [...milestones];
     sortedMilestones = sortedMilestones.sort((a, b) => {
-        return new Date(a.deadline).getDate() - new Date(b.deadline).getDate();
+        return new Date(a.deadline).getTime() - new Date(b.deadline).getTime();
     });
-    sortedMilestones.reverse();
+    // sortedMilestones.reverse();
     console.log(sortedMilestones);
 
     return sortedMilestones.map((milestone: Milestone) => {
         return (
             <div key={milestone._id}>
-                <div className="mt-8 flex items-center justify-start">
+                <div className="mt-4 flex items-center justify-start">
                     <div className="rounded-full bg-blue-400 w-10 h-10"></div>
                     <div className="ml-4 text-gray-500">
                         {new Date(milestone.deadline).toLocaleDateString(
@@ -30,9 +30,9 @@ const MilestoneList = ({ milestones }: Props) => {
                     </div>
                 </div>
                 <div className="flex items-center justify-start w-full">
-                    <div className="w-10"></div>
-                    <div className="ml-4 w-5/6">
-                        <div className="bg-white rounded-lg mt-4 pb-1">
+                    {/* <div className="w-10 h-full"></div> */}
+                    <div className="border-l-2 border-[#BFDBFE] pl-4 mt-4 ml-5 w-5/6">
+                        <div className="bg-white rounded-lg pb-1">
                             <div className="flex justify-between">
                                 <div className="font-semiold text-md p-4">
                                     {milestone.title}
